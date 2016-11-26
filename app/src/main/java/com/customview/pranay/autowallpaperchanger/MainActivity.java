fb3a4be89a70918f822b1a48ee9da20d2be652d7
pranay1494 <pranay1494@gmail.com>
2016-11-26 21:05:01 +0530

    + app icon and gson added

--- app/src/main/java/com/customview/pranay/autowallpaperchanger/MainActivity.java
+++ app/src/main/java/com/customview/pranay/autowallpaperchanger/MainActivity.java
@@ -4,8 +4,6 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
